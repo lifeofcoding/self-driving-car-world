4e903955f9bcae44d748e3dda37296306c326d1d
LifeOfCoding <lifeofcoding@gmail.com>
2023-12-16 22:31:33 -0500

    editor work

--- js/primitives/point.js
+++ js/primitives/point.js
@@ -4,11 +4,32 @@ class Point {
     this.y = y;
   }
 
-  draw(ctx, size = 18, color = "black") {
+  draw(
+    ctx,
+    { size = 18, color = "black", outline = false, fill = false } = {}
+  ) {
     const rad = size / 2;
     ctx.beginPath();
     ctx.fillStyle = color;
     ctx.arc(this.x, this.y, rad, 0, 2 * Math.PI);
     ctx.fill();
+    if (outline) {
+      ctx.beginPath();
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = "yellow";
+      ctx.arc(this.x, this.y, rad * 0.6, 0, 2 * Math.PI);
+      ctx.stroke();
+    }
+
+    if (fill) {
+      ctx.beginPath();
+      ctx.fillStyle = "yellow";
+      ctx.arc(this.x, this.y, rad * 0.4, 0, 2 * Math.PI);
+      ctx.fill();
+    }
+  }
+
+  equals(point) {
+    return this.x === point.x && this.y === point.y;
   }
 }
--- js/primitives/segment.js
+++ js/primitives/segment.js
@@ -4,12 +4,22 @@ class Segment {
     this.p2 = p2;
   }
 
-  draw(ctx, width = 2, color = "black") {
+  draw(ctx, { width = 2, color = "black", dash = [] } = {}) {
     ctx.beginPath();
     ctx.lineWidth = width;
     ctx.strokeStyle = color;
+    ctx.setLineDash(dash);
     ctx.moveTo(this.p1.x, this.p1.y);
     ctx.lineTo(this.p2.x, this.p2.y);
     ctx.stroke();
+    ctx.setLineDash([]);
+  }
+
+  equals(seg) {
+    return this.includes(seg.p1) && this.includes(seg.p2);
+  }
+
+  includes(point) {
+    return this.p1.equals(point) || this.p2.equals(point);
   }
 }
